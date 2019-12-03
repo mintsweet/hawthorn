5519eb455e73e150de81c2e142ad62f73e512b47
mintsweet <0x1304570@gmail.com>
2019-12-03 11:36:52 +0800

    feat: add init db script in x-run

--- scripts/x-run.sh
+++ scripts/x-run.sh
@@ -6,3 +6,7 @@ docker-compose up -d
 echo "-----DOCKER 镜像启动：结束-----"
 
 # MongoDB 初始化
+echo "-----MongoDB 初始化：开始-----"
+docker cp scripts/db hawthorn-mongo:/tmp/
+docker exec -it hawthorn-mongo bash -c 'cd /tmp/db && mongo mongodb://127.0.0.1/hawthorn?readPreference=primary index.js'
+echo "-----MongoDB 初始化：结束-----"
